faf83081812ebe859610cbd4b338b8d94947cd57
Aiman-Developer <aimansiddiqui77254@gmail.com>
2024-11-18 10:41:58 -0800

    task done

---
+++ app.js
@@ -0,0 +1,110 @@
+
+//Chapter # 12-13//
+
+//Question # 01//
+
+// let input = prompt("Enter a letter");
+// const upperCase = input.toUpperCase();
+// const lowerCase = input.toLowerCase();
+// // const numInput = Number(input)
+// if (input === upperCase || lowerCase){
+//     if (input === upperCase) {
+//         console.log(input + 'is an uppercase letter');
+//      } else  if (input === lowerCase) {
+//         console.log(input + 'is a lowercase letter');
+//     } else {
+//         console.log(input + 'is a special character');
+//     }
+// }
+
+//Question # 02//
+
+// let num1 = prompt('Enter num1 value');
+// let num2 = prompt('Enter num2 value');
+// if (num1>num2){
+//     console.log('The larger number is' + num1);
+// }else if (num2 >num1){
+//     console.log('the larger number is' + num2);
+    
+// } else if(num1===num2){
+//     console.log('Both are equals');
+    
+// } else {
+//     console.log('Please enter a valid number');
+    
+// }
+
+//Question # 03//
+
+// let number = prompt('Enter a number');
+// if (number > 0) {
+//     console.log('Number is positive');
+// } else if (number < 0) {
+//     console.log('Number is negative');
+// } else if (number === 0) {
+//     console.log('Number is zero');
+// }
+
+//Question # 04//
+
+// let letter = prompt('Write a letter');
+//  function myfunction(letter) {
+// let vowels = ["a", "e", "i", "o", "u"];
+// for(var i in vowels){
+//     if(letter === i){
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+// }
+
+//Question # 05//
+
+// let pass1 = 123;
+// let user = prompt('Enter your password');
+// let user2 = prompt('Confirm password');
+// if (user===pass1) {
+//     console.log('Correct! The password you entered matches the original password');
+    
+// } else if (user2===pass1) {
+//     console.log('Correct! The password you entered matches the original password');
+    
+// } else if (user2===user) {
+//     console.log('Correct! The password you entered matches the original password');
+    
+// }else if (user!=user2) {
+//     console.log('Incoorect password');
+    
+// } else {
+//     console.log('Please enter your password');
+    
+// } 
+
+//Question # 06//
+
+//  let greeting;
+// let hour = prompt("Time")
+// if (hour < 18) {
+//  console.log("Good day");
+ 
+// }else {
+// console.log("Good evening");
+
+// }
+
+//Question # 07//
+
+// let time = prompt('Guess the time like 0000')
+// if (time >= 0 && time < 1200) {
+// console.log('Good morning');
+// } else if (time >= 1200 && time <1700) {
+//     console.log('Good afternoon');
+// } else if (time >= 1700 && time <2100) {
+//     console.log('Good evening');
+    
+// } else if (time>= 2100 && time < 2359) {
+//     console.log('Good night');
+    
+// } 
+
